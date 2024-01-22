47d6f0e1ea8159235dbb8643bb21f0b86cefa058
Philip Lundin <lundiin@live.se>
2024-01-22 17:19:29 +0100

    style: media query

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,7 @@ function Content() {
                 <h1 className="content-heading">Vem är jag</h1>
                 <p className="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             </div>
-            <img className="profile-image" src={mange} />
+            <img className="profile-image" src={mange} alt="Profile" />
         </div>
     );
 }
