5c34ce186db07f8b3941bf9e61aaf59ee3f089a9
Philip Lundin <lundiin@live.se>
2024-02-08 16:41:12 +0100

    feat: add anchors links to nav & scroll behavior

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import mange from '../images/mangemange.jpg';
 
 function Content() {
     return (
-        <div className="content">
+        <div className="content" id="content">
             <div className="content-text_box">
                 <h1 className="content-heading">Vem är jag</h1>
                 <p className="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,9 +4,9 @@ function Nav() {
     return (
         <nav className="nav">
             <ul className="nav-flex">
-                <li><a href="">Vandring</a></li>
-                <li><a href="">Om oss</a></li>
-                <li><a href="">Kontakt</a></li>
+                <li><a href="#hero">Vandring</a></li>
+                <li><a href="#content">Om oss</a></li>
+                <li><a href="#contact">Kontakt</a></li>
             </ul>
         </nav>
     );
