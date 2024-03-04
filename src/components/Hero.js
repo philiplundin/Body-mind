27353b01173f1e6d6cc606f9cf40d9d141f0791f
Philip Lundin <lundiin@live.se>
2024-03-04 07:48:21 +0100

    feat: added arrow to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,12 @@ function Hero() {
   return (
     <div className="hero" id="hero">
       <div className="hero-box">
-      <h1 className="hero-heading">Upplev naturen</h1>
-      <p className="hero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <h1 className="hero-heading">Upplev naturen</h1>
+        <p className="hero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
+      <a href='#content'><i className='arrow' /></a>
     </div>
   );
-} 
+}
 
 export default Hero;
